b922d1c7d7d10ab201a22223a18c6f69e1c9dbca
blast66 <113650618+blast66@users.noreply.github.com>
2022-10-07 10:48:52 +0800

    Add files via upload

---
+++ test7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test7 {
+    public static void main(String []args) {
+		int grade = 9;
+		
+		while (grade >= 0) {
+			System.out.printf("%d %n", grade);
+			grade = grade - 1 ;
+		}
+	}
+}
